c1a68893d45bccd08131196baf65fdb40ec98d57
ppond454 <ppond.454@gmail.com>
2021-12-12 02:54:47 +0700

    .

--- routes/blog.js
+++ routes/blog.js
@@ -4,8 +4,6 @@ const db = require("monk")("localhost:27017/VaccineDB")
 const { check, validationResult, Result, body } = require("express-validator")
 // const Blogs=require('../models/blogs')
 
-
-
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   const collection = db.get("blogs")
@@ -27,11 +25,10 @@ router.get("/", function (req, res, next) {
 router.get("/edit/(:id)", (req, res, next) => {
   let id = req.params.id
   const collection = db.get("blogs")
-
   collection.find({ _id: id }).then((doc) => {
     res.render("edit", {
       title: "แก้ไขข้อมูล",
-      id: doc._id,
+      id: id,
       name: doc[0].name,
       num_vaccine: doc[0].num_vaccine,
       name_vaccine: doc[0].name_vaccine,
@@ -40,17 +37,57 @@ router.get("/edit/(:id)", (req, res, next) => {
   })
 })
 
-router.post("/update/:id",(req, res, next) => {
-    const id = req.params.id
-    console.log(id);
-  }
-)
+router.post("/edit/(:id)", [
+  check("name", "กรุณาป้อนชื่อ-นามสกุล").not().isEmpty(),
+  check("num_vaccine", "กรุณาใส่จำนวนวัคซีน").not().isEmpty(),
+  check("name_vaccine", "กรุณาป้อนประเภทวัคซีน").not().isEmpty(),
+  check("detail", "กรุณาป้อนอาการ").not().isEmpty(),
+],(req, res, next) => {
+
+  const collection = db.get("blogs")
+  const id = req.params.id
+
+  const result = validationResult(req)
+    var errors = result.errors
+    if (!result.isEmpty()) {
+      collection.find({ _id: id }).then((doc) => {
+        res.render("edit", {
+          errors: errors,
+          title: "แก้ไขข้อมูล",
+          id: id,
+          name: doc[0].name,
+          num_vaccine: doc[0].num_vaccine,
+          name_vaccine: doc[0].name_vaccine,
+          detail: doc[0].detail,
+        })
+      })
+
+    }else{
+      collection
+      .findOneAndUpdate(
+        { _id: id },
+        {
+          $set: {
+            name: req.body.name,
+            num_vaccine: req.body.num_vaccine,
+            name_vaccine: req.body.name_vaccine,
+            detail: req.body.detail,
+          },
+        }
+      )
+      .then(() => {
+        res.location("/blog/")
+        res.redirect("/blog/")
+      })
+    }
+
+})
 
 // delete
 router.get("/delete/(:id)", (req, res, next) => {
   const collection = db.get("blogs")
   let id = req.params.id
-  collection.remove({ _id: id }).then(()=>{
+  collection.remove({ _id: id }).then(() => {
     res.location("/blog")
     res.redirect("/blog")
   })
@@ -96,11 +133,8 @@ router.post(
           }
         }
       )
-     
     }
-   
   }
 )
-
 
 module.exports = router
